56aa0ce653c783d2ba29cfdd77ce721d79e86ebb
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-13 15:37:58 +0545

    Create find element in tuple.py

---
+++ find element in tuple.py
@@ -0,0 +1,7 @@
+colors = ("red", "green", "blue")
+a=input("Enter a color of your choice:")
+if a in colors:
+  print("The color",a,"can be used")
+else:
+  print("The color",a,"is not found")
+                                              
