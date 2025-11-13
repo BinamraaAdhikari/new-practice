377c01625cca42438d61b5881ed26f6b115d1d7c
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-13 15:12:57 +0545

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,18 @@
+a=[]
+b=float(input("The marks Rahul obtained is:") )   
+a.append(b)      
+b1=float(input("The marks Raj obtained is:")  )  
+a.append(b1)   
+b2=float(input("The marks Ram obtained is:")    )
+a.append(b2)   
+b3=float(input("The marks Rama obtained is:")    )
+a.append(b3)   
+b4=float(input("The marks Rajesh obtained is:")    )
+a.append(b4)   
+b5=float(input("The marks Raggy obtained is:")    )
+a.append(b5)   
+a.sort()
+c=(b+b1+b2+b3+b4+b5)/6
+print("The sorted marks obtained by the six students are:",a)
+print("So the average marks among the six students is",c)                                                     
+                                                                 
