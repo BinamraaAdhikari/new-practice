7d8a53ba070b7bdbe93bfc381389981157f618d1
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-17 14:14:05 +0545

    Update very basic if else2.py

--- very basic if else2.py
+++ very basic if else2.py
@@ -3,7 +3,7 @@ b=int(input("Enter the marks obtained in Math:"))
 c=int(input("Enter the marks obtained in Health:"))
 total=(100)*(a+b+c)/300
 
-if total>=40:
+if total>=40 and a>33 and b>33 and c>33:
     print("You passed with",total,"%")
 else:
     print("You failed with",total,"%")
