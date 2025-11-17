382926d4be066dac2d6277a700510ee1629f512a
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-17 10:56:46 +0545

    Create fetch from dictionary.py

---
+++ fetch from dictionary.py
@@ -0,0 +1,10 @@
+marks={"Harry":55,
+       "Raj":66,
+       "Shiv":77}
+a=input("Enter a name:")
+
+if a in marks:
+    print(a,"scored",marks[a])
+else:
+    print("The name doesnt exist")
+                                     
