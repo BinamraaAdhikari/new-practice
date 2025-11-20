42e556ec6e7fe94e2a2c6d627c2d78bca93e649a
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-20 12:22:10 +0545

    Create def small example.py

---
+++ function/def small example.py
@@ -0,0 +1,6 @@
+def greatest(a,b,c):
+    return max(a,b,c)
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+print("The greatest number is", greatest(a,b,c))
