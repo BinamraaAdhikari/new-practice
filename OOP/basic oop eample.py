f08318069cf8a772e111ac9daff22a08b377a197
BinamraaAdhikari <adhikarybinamraaa@gmail.com>
2025-11-21 18:53:38 +0545

    Update basic oop eample.py

--- OOP/basic oop eample.py
+++ OOP/basic oop eample.py
@@ -1,7 +1,10 @@
 class car(): 
 def __init__(self,name,color,price): 
-  self.name=name self.color=color self.price=price 
-  def getinfo(self): print("The color of ",self.name,"car is",self.color,"and it cost about Rs",self.price) 
+  self.name=name 
+  self.color=color 
+  self.price=price 
+  def getinfo(self): 
+    print("The color of ",self.name,"car is",self.color,"and it cost about Rs",self.price) 
     
 car1=car("Harry","Red",120000) 
 car2=car("Dogesh","Green",344200) 
